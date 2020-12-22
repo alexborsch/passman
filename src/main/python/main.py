9a3d164423c398263c0f82353a01a601ce07fd0c
alexborsch <a.borsch@ukr.net>
2020-12-22 13:37:23 +0200

    commit

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -9,7 +9,10 @@ import sys
 import json
 import sqlite3
 import os
+from functools import partial
 
+app_name = 'passman alpha'
+database = 'data/database.db'
 
 with open('settings.json') as f:
     file_content = f.read()
@@ -24,7 +27,7 @@ def clear():
 	text.delete('1.0', END)
 
 def create_new_db():
-    conn = sqlite3.connect("data/database.db") # или :memory: чтобы сохранить в RAM
+    conn = sqlite3.connect(database) # или :memory: чтобы сохранить в RAM
     cursor = conn.cursor()
     
     cursor.execute("""CREATE TABLE IF NOT EXISTS `passwords_db` (
@@ -35,7 +38,7 @@ def create_new_db():
     conn.commit()
 
 def load_list():
-    conn = sqlite3.connect("data/database.db")
+    conn = sqlite3.connect(database)
     #conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     
@@ -43,7 +46,7 @@ def load_list():
         box.insert(0, i)
 
 def load_data(select):
-    conn = sqlite3.connect("data/database.db")
+    conn = sqlite3.connect(database)
     #conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     
@@ -54,14 +57,19 @@ def load_data(select):
 
 def delete_point(selected):
     
-    conn = sqlite3.connect("data/database.db")
+    conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    #sql = "DELETE FROM passwords_db WHERE title=:point", point
-    cursor.execute("DELETE FROM passwords_db WHERE title=:point", selected)
+    cursor.execute("DELETE FROM passwords_db WHERE title = \"{}\"".format(selected))
     conn.commit()
+    messagebox.showinfo("Information", selected+" was delete")
+    box.delete(0, END)
+    text.delete('1.0', END)
+    load_list()
 
 def edit_point():
     pass
+
+
 
 def onselect(event):
     global selected
@@ -72,22 +80,66 @@ def onselect(event):
     print(selected)
     load_data(value)
 
-
-  
+ 
 def do_popup(event): 
     try: 
         m.tk_popup(event.x_root, event.y_root) 
     finally: 
         m.grab_release() 
-  
 
 
+def add_pass():
+
+    
+
+    tkWindow = Tk()  
+    tkWindow.geometry('330x130+350+350')
+    tkWindow.resizable(width=False, height=False)
+    
+    tkWindow.title(app_name+' | Add password')
+
+    pass_title_Label = Label(tkWindow, text="Title password: ").grid(row=0, column=0)
+    pass_title = StringVar()
+    pass_title_Entry = Entry(tkWindow, textvariable=pass_title, width=35).grid(row=0, column=1)
+
+    pass_username_Label = Label(tkWindow, text="Username: ").grid(row=1, column=0)
+    pass_username = StringVar()
+    pass_username_Entry = Entry(tkWindow, textvariable=pass_username, width=35).grid(row=1, column=1)
+
+    pass_password_Label = Label(tkWindow, text="Password: ").grid(row=2, column=0)
+    pass_password = StringVar()
+    pass_password_Entry = Entry(tkWindow, textvariable=pass_password, width=35).grid(row=2, column=1)
+
+    pass_description_Label = Label(tkWindow, text="Description: ").grid(row=3, column=0)
+    pass_description = StringVar()
+    pass_description_Entry = Entry(tkWindow, textvariable=pass_description, width=35).grid(row=3, column=1)
+
+    def saveData(pass_title, pass_username, pass_password, pass_description):
+
+        title = str(pass_title.get())
+        username = str(pass_username.get())
+        password = str(pass_password.get())
+        descript = str(pass_description.get())
+        data = title + " account information\nUsername: "+ username + "\nPassword: "+ password +"\nDescription: "+ descript
+        print(title + '  ' + data)
+        '''
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO passwords_db (title, data) VALUES (\"{}\",\"{}\")".format(title, data))
+        conn.commit()
+        '''
+
+    validateSave = partial(saveData, pass_title, pass_username, pass_password, pass_description)
+    saveButton = Button(tkWindow, text="Save", command=validateSave).grid(row=4, column=0)
+    mainloop()
+
+    
 
 tkWindow = Tk()  
 tkWindow.geometry('630x380+300+300')
 tkWindow.resizable(width=True, height=True)
 #tkWindow.iconbitmap('Icon.ico')  
-tkWindow.title('passman alpha')
+tkWindow.title(app_name)
 
 
 
@@ -98,7 +150,7 @@ scroll = Scrollbar(command=box.yview)
 scroll.pack(side=LEFT, fill=Y)
 box.config(yscrollcommand=scroll.set)
 
-if os.path.exists("data/database.db"):
+if os.path.exists(database):
     load_list()
 
 box.bind('<<ListboxSelect>>', onselect)
@@ -116,7 +168,6 @@ text.pack()
 # menubar
 menuBar = Menu(tkWindow)
 fileMenu = Menu(menuBar)
-fileMenu.add_command(label="Add")
 fileMenu.add_command(label="Create DB", command=create_new_db)
 fileMenu.add_command(label="Import")
 fileMenu.add_command(label="Export")
@@ -136,8 +187,9 @@ tkWindow.config(menu=menuBar)
 
 # right click menu
 m = Menu(tkWindow, tearoff = 0) 
+m.add_command(label ="Add", command=add_pass) 
 m.add_command(label ="Edit", command=edit_point) 
-m.add_command(label ="Delete", command=lambda: delete_point(selected[0])) 
+m.add_command(label ="Delete", command=lambda: delete_point(selected)) 
 
 
 tkWindow.mainloop()
